3a65a0ac8b1a111850492b5b95bde4a69e9f77b0
Anggarda Saputra Wijaya <anggarda.wijaya410@gmail.com>
2024-09-02 09:56:41 +0700

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #author : Anggarda Saputra Wijaya. anggarda.wijaya@outlook.com. github.com/anggardawjy
-#version : 0.1
+#version : 0.3
 #about : debian 10 docker-ce install
-
 apt install sudo -y
 apt install curl -y
-sudo apt-get update
+sudo apt-get update -y
 sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
+sudo apt update -y
 apt-cache policy docker-ce
 sudo apt install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo systemctl status docker
