6d4381f94efe07678fd097ee12e30bfb32bbe9a6
Anggarda Saputra Wijaya <anggarda.wijaya410@gmail.com>
2024-09-02 15:31:16 +0700

    Delete docker-shellinabox directory

--- docker-shellinabox/shellinabox.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 21112:21112 -p 23332:23332 -e SIAB_USER=shellina -e SIAB_PASSWORD=baseball -e SIAB_HOME=/home/shellina -e SIAB_SSL=false -e SIAB_SUDO=true  sspreitzer/shellinabox:latest
