64b6e7a236f1d1f573378bece5b1e5ac2f9e4bf7
Anggarda Saputra Wijaya <anggarda.wijaya410@gmail.com>
2024-09-02 15:06:14 +0700

    Update shellinabox.sh

--- docker-shellinabox/shellinabox.sh
+++ docker-shellinabox/shellinabox.sh
@@ -1,4 +1,5 @@
-docker run -d -p 21112:21112 -p 23332:23332 -e SIAB_USER=shellina -e 
+docker run -d -p 21112:21112 -p 23332:23332 -e 
+SIAB_USER=shellina -e 
 SIAB_PASSWORD=baseball -e 
 SIAB_HOME=/home/shellina -e SIAB_SSL=false -e 
 SIAB_SUDO=true  sspreitzer/shellinabox:latest
